f013dda7b56349f5ab8c63489d0771d4312396ca
Arsema <arsema.solomonbarhane>
2026-05-21 00:04:06 +0200

    eerste query

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PlanetController extends Controller
 {
-    //
+    public function index()
+    {
+        // SELECT * FROM planets
+        $planets = DB::table('planets')->get();
+        return $planets;
+    }
+
+    public function show($id)
+    {
+        // SELECT * FROM planets WHERE id = ?
+        $planet = DB::table('planets')->where('id', $id)->first();
+        return $planet;
+    }
 }
+
+?>
+
